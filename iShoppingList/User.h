fe5ecf2ccf873e939132adf2836b7a5c58d9b358
thierry allard saint albin <thierry.allardsaintalbin@gmail.com>
2015-02-22 11:20:33 +0100

    Classe user pour avoir le JSon faire méthode description

---
+++ iShoppingList/User.h
@@ -0,0 +1,34 @@
+//
+//  User.h
+//  iShoppingList
+//
+//  Created by thierry allard saint albin on 22/02/2015.
+//  Copyright (c) 2015 Tasa&Fllo. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface User : NSObject
+{
+@private
+    NSString* UserName_;
+    NSString* mailUser_;
+    NSString* passUser_;
+    NSString* IdIphone_;
+}
+
+-(instancetype)init;
+
+-(instancetype)initWithName:(NSString*)UserName AndMailUser:(NSString*)mailUser andPassUser:(NSString*)passUser andIdIphone:(NSString*)idIphone;
+
+-(instancetype)initWithMailUser:(NSString*)mailUser andPassUser:(NSString*)passUser;
+
+@property(strong, nonatomic) NSString* UserName;
+@property(strong,nonatomic) NSString* mailUser;
+@property(strong,nonatomic) NSString* passUser;
+@property(strong,nonatomic) NSString* IdIphone;
+
+-(NSString*)description;
+
+
+@end
---
+++ iShoppingList/User.m
@@ -0,0 +1,13 @@
+//
+//  User.m
+//  iShoppingList
+//
+//  Created by thierry allard saint albin on 22/02/2015.
+//  Copyright (c) 2015 Tasa&Fllo. All rights reserved.
+//
+
+#import "User.h"
+
+@implementation User
+
+@end
