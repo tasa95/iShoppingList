8cb6bbef77a3b190139259d3c574111337ea1b64
marc michaud <michaud.java@gmail.com>
2015-03-15 02:00:01 +0100

    classe de factorisation dite moi si vous voyez eu est mon erreur je suis encore bloqué..

---
+++ iShoppingList/ParamsURL.h
@@ -0,0 +1,13 @@
+//
+//  ParamsURL.h
+//  iShoppingList
+//
+//  Created by ap7 on 14/03/2015.
+//  Copyright (c) 2015 Tasa&Fllo. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface ParamsURL : NSObject
+
+@end
---
+++ iShoppingList/ParamsURL.m
@@ -0,0 +1,13 @@
+//
+//  ParamsURL.m
+//  iShoppingList
+//
+//  Created by ap7 on 14/03/2015.
+//  Copyright (c) 2015 Tasa&Fllo. All rights reserved.
+//
+
+#import "ParamsURL.h"
+
+@implementation ParamsURL
+
+@end
